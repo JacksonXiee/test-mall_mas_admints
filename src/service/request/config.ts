4de243742dff93cf5f319cfd38cfbecf7741f140
JacksonXiee <1289168386@qq.com>
2022-09-11 20:12:40 +0800

    commit信息

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -5,9 +5,9 @@ const TIME_OUT = 10000
 if (import.meta.env.DEV) {
   BASE_URL = '/api'
 } else if (import.meta.env.PROD) {
-  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_URL = 'http://152.136.185.210:5000'
 } else {
-  BASE_URL = 'http://coderwhy.org/test'
+  BASE_URL = 'http://152.136.185.210:5000'
 }
 
 export { BASE_URL, TIME_OUT }
